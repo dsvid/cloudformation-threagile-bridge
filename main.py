f71ecf8739269e9f07e4fe949fa9c5b169ce0611
dsvid <davidlarochephoto@gmail.com>
2021-10-30 19:20:49 +1100

    the trust_boundaries are now stored in a nested dictionary rather than a dictionary inside a list this should make it easier to add extra key/values later when we add the ability to parse technical and data assets as we'll want to associate those with the relevant trust boundary for example we could add assets to the trust boundary of PublicSubnet1 like:
    dict_temp = {'assets': ['abc', 'bcd']}
    trust_boundaries['PublicSubnet1'].update(dict_temp)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ path_to_json = os.path.join("test_files", "vpc_cf_example.json")
 with open(path_to_json) as cf_json:
     cf_data = json.load(cf_json)
 
-trust_boundaries = []
+trust_boundaries = {}
 
 for i in cf_data['Resources']:
     # if the resource is a VPC
@@ -19,30 +19,22 @@ for i in cf_data['Resources']:
                 if cf_data['Resources'][j]['Properties']['VpcId']['Ref'] == i:
                     nested_boundaries.append(j.lower())
         # the nested trust boundaries that we just found will be included in the append
-        trust_boundaries.append({
-            i: {
+        trust_boundaries[i] = {
                 'id': i.lower(), 
                 'type':'network-cloud-security-group', 
                 # 'tags':'', 
                 # 'technical_assets_inside':'', 
                 'trust_boundaries_nested': nested_boundaries
-            }})
+            }
 
     elif cf_data['Resources'][i]['Type'] == 'AWS::EC2::Subnet':
         # if the resource is a subnet, then just add it as a trust boundary with no nested boundaries
-        trust_boundaries.append({
-            i: {
+        trust_boundaries[i] = {
                 'id': i.lower(), 
                 'type':'network-cloud-security-group', 
                 # 'tags':'', 
                 # 'technical_assets_inside':'', 
                 # 'trust_boundaries_nested':''
-            }})
+            }
 
-print(trust_boundaries)
-
-for i in trust_boundaries:
-    # this is the output that will go in the trust_boundaries section of the threagile input
-    # right now it will still need to be manually massaged just a tad, but we can fix that later
-    # within the trust boundaries section we also need to add mapping to technical assets, once we figure out how to approach those
-    print(yaml.dump(i))
+print(yaml.dump(trust_boundaries, default_flow_style=False))
