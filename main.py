3b8af0f7d873858fbfd4b2dfadb7b3b60fdffa5a
dsvid <davidlarochephoto@gmail.com>
2021-11-09 00:13:36 +1100

    added rudimentary support for communication links this works by adding a "can_communicate_with" attribute to the technical asset in the resource rules.
    currently only supports technical assets, and can only define one
    property name and nested property name for each asset type.

--- main.py
+++ main.py
@@ -72,7 +72,8 @@ def parse_cf_resources_for_trust_boundaries(cf_template):
                         print(j, "has no properties")
             # the nested trust boundaries that we just found will be included in the append
             trust_boundaries[i] = {
-                    'id': i.lower(), 
+                    'id': i.lower(),
+                    'aws-type': aws_type, # not needed by threagile, but useful for reference
                     'type': resource_rules['outer-trust-boundaries'][aws_type]['type'], 
                     'tags': [resource_rules['outer-trust-boundaries'][aws_type]['tags']], 
                     # 'technical_assets_inside':'', 
@@ -84,6 +85,7 @@ def parse_cf_resources_for_trust_boundaries(cf_template):
             # if the resource is a subnet, then just add it as a trust boundary with no nested boundaries
             trust_boundaries[i] = {
                     'id': i.lower(), 
+                    'aws-type': aws_type, # not needed by threagile, but useful for reference
                     'type': resource_rules['inner-trust-boundaries'][aws_type]['type'], 
                     'tags': [resource_rules['inner-trust-boundaries'][aws_type]['tags']]
                     # 'technical_assets_inside':'', 
@@ -98,7 +100,8 @@ def parse_cf_resources_for_technical_assets(cf_template):
         aws_type = cf_data['Resources'][i]['Type']
         if aws_type in resource_rules['technical-assets']:
             technical_assets[i] = {
-                'id': i.lower(), 
+                'id': i.lower(),
+                'aws-type': aws_type, # not needed by threagile, but useful for reference
                 'type': resource_rules['technical-assets'][aws_type]['type'], # values: external-entity, process, datastore
                 'usage': resource_rules['technical-assets'][aws_type]['usage'], # values: business, devops
                 'size': resource_rules['technical-assets'][aws_type]['size'], # values: system, service, application, component
@@ -124,6 +127,43 @@ def parse_cf_resources_for_technical_assets(cf_template):
             except KeyError:
                 print(i, "has no properties")
 
+def parse_for_communication_links(cf_template):
+    # trawling the template again, but it's just a bit easier logically, since we already have the technical assets to start
+    # if trawling multiplte times has a substantial performance impact then we can optimize later, but I think it's probably fine
+    for i in cf_data['Resources']:
+        aws_type = cf_data['Resources'][i]['Type']
+        links = {'communication_links': {}}
+        if aws_type in resource_rules['technical-assets']:
+            if 'can_communicate_with' in resource_rules['technical-assets'][aws_type]:
+                # print(resource_rules['technical-assets'][aws_type]['can_communicate_with'])
+                for j in technical_assets:
+                    asset_type = technical_assets[j]['aws-type']
+                    if asset_type in resource_rules['technical-assets'][aws_type]['can_communicate_with']:
+                        property_name = resource_rules['technical-assets'][aws_type]['can_communicate_with'][asset_type]['property_name']
+                        property_name_nested =  resource_rules['technical-assets'][aws_type]['can_communicate_with'][asset_type]['property_name_nested']
+                        print(property_name)
+                        # property_name = cf_data['Resources'][j]['Properties'][aws_type][property_name]
+                        try:
+                            if type(cf_data['Resources'][i]['Properties'][property_name][property_name_nested]) is list:
+                                link_target = cf_data['Resources'][i]['Properties'][property_name][property_name_nested][0].lower()
+                            else:
+                                link_target = cf_data['Resources'][i]['Properties'][property_name][property_name_nested].lower()
+                            print("link target is: ", link_target)
+                            link_name = 'Link to ' + link_target
+                            link = {link_name: {
+                                'target': link_target,
+                                'protocol': 'https',
+                                'authentication': 'none',
+                                'authorization': 'none',
+                                'usage': 'business'
+                                    }
+                                }
+                            links['communication_links'].update(link)
+                            
+                            # add the link to the asset
+                            technical_assets[i].update(links)
+                        except Exception as e: print("error occured: ", repr(e), "j value: ", j, "i value: ", i, "property_name_nested: ", property_name_nested)
+
 # run the parser functions and print out the status 
 # (maybe better to use logging, but print is fine for now)
 print("parsing CF template for trust boundaries")
@@ -131,6 +171,9 @@ parse_cf_resources_for_trust_boundaries(cf_data)
 
 print("parsing CF template for trust technical assets")
 parse_cf_resources_for_technical_assets(cf_data)
+
+print("parsing CF template for communication links")
+parse_for_communication_links(cf_data)
 
 
 # if the existing yaml was empty for the parts we're going to add to, then initialize them
